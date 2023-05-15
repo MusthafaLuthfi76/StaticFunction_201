1ac9098a635ef7993705bc75fd500457d340fd61
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-15 20:00:33 +0700

    class mahasiswa

---
+++ Konstruktor/Konstruktor.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private :
+    int nim;
+    string nama;
+public :
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswan(int iNim, string iNama);
+    void cetak();
+};
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
