acd0f02a451e30eff1c03003ad1a67914b544ccf
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2023-05-15 15:55:36 +0700

    void setID

--- mbStaticFunction1/mbStaticFunction1.cpp
+++ mbStaticFunction1/mbStaticFunction1.cpp
@@ -20,6 +20,10 @@ public :
 
 int mahasiswa::nim = 0;
 
+void mahasiswa::setID() {
+    id = ++nim;
+}
+
 void mahasiswa::printAll() {
     cout << "ID = " << id << endl;
     cout << "Nama = " << nama << endl;
